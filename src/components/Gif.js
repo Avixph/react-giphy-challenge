12e20b36a81cf253b53de34781a3452248125bbc
Angel B. Fernandez <25628695+Avixph@users.noreply.github.com>
2021-02-02 07:56:34 -0500

    complete randomizer edit

--- src/components/Gif.js
+++ src/components/Gif.js
@@ -2,9 +2,9 @@ export default function Gif({ gif }) {
   return (
     <div className="app-view">
       <div className="single-gif">
-        <h5>Title:{gif.title ? gif.title : "Unknown"}</h5>
+        <h5>Title: {gif.title ? gif.title : "Unknown"}</h5>
         <img src={gif.image_url} alt="a gif" />
-        <p>By: {gif.username}</p>
+        <p>By: {gif.username ? gif.username : "Unknown"}</p>
       </div>
     </div>
   );
