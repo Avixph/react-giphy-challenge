9f31e089bdbdc25ed2ebc76f7e1fde317d2ce4b9
Angel B. Fernandez <25628695+Avixph@users.noreply.github.com>
2021-02-02 16:04:17 -0500

    code cleanup

--- src/components/MainGif.js
+++ src/components/MainGif.js
@@ -1,19 +1,15 @@
 import React, { useState, useEffect } from "react";
 import { RandomGifAPI } from "../services/giphy_API";
 import Gif from "./Gif";
-// import SearchGif from "./SearchGif";
 
 export default function MainGif() {
   const [gif, setGif] = useState([]);
-  // const [loading, setLoading] = useState(true);
 
   console.log(gif);
 
   const fetchRandomGif = async () => {
-    // setLoading(true);
     const randGif = await RandomGifAPI();
     setGif(randGif.data);
-    // setLoading(false);
   };
   console.log(fetchRandomGif);
 
@@ -24,9 +20,8 @@ export default function MainGif() {
   return (
     <main className="app-main">
       <h3 className="app-main">I'm going to show random gifs!</h3>
-      {/* <SearchGif /> */}
       <button onClick={fetchRandomGif}>Gif Me</button>
-      {gif ? <Gif gif={gif} /> : <LoadingGif />}
+      <Gif gif={gif} />
     </main>
   );
 }
